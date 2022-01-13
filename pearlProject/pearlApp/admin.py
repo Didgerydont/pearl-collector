062f1368f894e66d8dba5f0441d0b4c0e964ec59
Will Prendergast <prendygast@hotmail.com>
2022-01-13 17:07:39 +0000

    added model

--- pearlProject/pearlApp/admin.py
+++ pearlProject/pearlApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Pearls
+
+@admin.register(Pearls)
+class PearlsAdmin(admin.ModelAdmin):
+    list_display = ("title", "list", "pub_date")
--- pearlProject/pearlApp/models.py
+++ pearlProject/pearlApp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+class Pearls(models.Model):
+    title = models.CharField(max_length=20)
+    list = models.TextField()
+    pub_date = models.DateTimeField()
+    
+
+    def __str__(self) -> str:
+        return f"{self.podcast_name}: {self.title}"
+
